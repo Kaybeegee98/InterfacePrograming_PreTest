2327eafb89effc84ff92aaaf5dcd74a21b2b967c
Kaybeegee98 <48079176+Kaybeegee98@users.noreply.github.com>
2019-09-10 13:42:59 +1000

    Created HTTYD2 for test class

--- TheMovieDB/src/app/movie-info/movie-info.component.ts
+++ TheMovieDB/src/app/movie-info/movie-info.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Movie } from '../movies';
 
 @Component({
   selector: 'app-movie-info',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-info.component.css']
 })
 export class MovieInfoComponent implements OnInit {
+
+  HTTYD2: Movie = {
+    Title: 'How To Train Your Dragon 2', 
+    Rating: 7.7,
+    ShortDescription: 'a',
+    Runtime: 102,
+    ReleaseDate: new Date('2014-6-19'),
+    MovieThumbnail: new URL('https://image.tmdb.org/t/p/w600_and_h900_bestv2/lRjOR4uclMQijUav4OjeZprlehu.jpg')
+  };
 
   constructor() { }
 
