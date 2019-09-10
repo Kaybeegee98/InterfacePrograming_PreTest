70b4c856c3df96a1fcf42797051b55d0b5bb00b6
Kaybeegee98 <48079176+Kaybeegee98@users.noreply.github.com>
2019-09-10 13:15:31 +1000

    Added Cover, Details and Times Components

--- TheMovieDB/src/app/app.module.ts
+++ TheMovieDB/src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { FooterBarComponent } from './footer-bar/footer-bar.component';
+import { CoverComponent } from './cover/cover.component';
+import { DetailsComponent } from './details/details.component';
+import { TimesComponent } from './times/times.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationBarComponent,
-    FooterBarComponent
+    FooterBarComponent,
+    CoverComponent,
+    DetailsComponent,
+    TimesComponent
   ],
   imports: [
     BrowserModule
